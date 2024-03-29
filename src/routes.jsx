b3b4c58dec294bdd51800d64ca19c1070e5843dc
Calvin <calvinsoares17@gmail.com>
2024-03-29 17:43:37 -0300

    feat: rotas atualizadas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,19 @@
-import { Routes, Route } from "react-router-dom"
 import StandardPage from "./components/StandardPage"
+import { Routes, Route } from 'react-router-dom'
 import DashBoard from "./pages/Dashboard"
+import Login from "./pages/Login"
 
 const Rotas = () => {
   return (
-    <>
-        <Routes>
-            <Route path="/" element={<StandardPage />}>
-                <Route path="/dashboard" element={<DashBoard />}/>
-            </Route>
-        </Routes>
-    </>
+
+      <Routes>
+          <Route element={<StandardPage />}>
+            <Route path="/dashboard" element={<DashBoard />} />
+          </Route>
+          
+        <Route path="/" element={<Login />} />
+      </Routes>
+
   )
 }
 
